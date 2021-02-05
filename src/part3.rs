08648cbf799b413542b060f1f7e9eee51e4942e6
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-02-05 18:21:54 -0500

    Add part3

--- src/part3.rs
+++ src/part3.rs
@@ -9,7 +9,7 @@
 
 // Remove these once you are done editing the file!
 #![allow(dead_code)]
-#![allow(unused_variables)]
+// #![allow(unused_variables)]
 
 /*
     Problem 1: Swap ints
@@ -21,18 +21,31 @@
     The Rust borrow checker may help avoid some possible bugs.
 */
 pub fn swap_ints(x1: &mut i32, x2: &mut i32) {
-    unimplemented!()
+    let temp = *x2;
+    *x2 = *x1;
+    *x1 = temp;
+}
+
+#[test]
+fn test_swap_ints() {
+    let mut x = 1;
+    let mut y = 2;
+    swap_ints(&mut x, &mut y);
+    assert_eq!(x, 2);
+    assert_eq!(y, 1);
+    // swap_ints(&mut x, &mut x);
+    // assert_eq!(x, 2);
 }
 
 /*
     Problem 2: String duplication
 */
-// #[test]
-// fn copy_string_test() {
-//     let str1 = String::from("foo");
-//     let str2 = str1;
-//     assert_eq!(str1, str2);
-// }
+#[test]
+fn copy_string_test() {
+    let str1 = String::from("foo");
+    let str2 = String::from(&str1);
+    assert_eq!(str1, str2);
+}
 // This test doesn't work. Fix it by copying strings properly.
 // Q1. What went wrong?
 
@@ -46,7 +59,11 @@ fn copy_int_test() {
 
 // Now implement the following function that duplicates a string n times.
 fn duplicate_string(s: &str, times: usize) -> Vec<String> {
-    unimplemented!()
+    let mut res = Vec::new();
+    for _ in 0..times {
+        res.push(String::from(s));
+    }
+    res
 }
 
 /*
@@ -57,22 +74,22 @@ fn duplicate_string(s: &str, times: usize) -> Vec<String> {
     it's called.
 */
 
-// fn copy_me(string: /* Change in here only*/ String) -> String {
-//     string.clone()
-// }
+fn copy_me(string: /* Change in here only*/ &str) -> String {
+    string.to_string()
+}
 
-// #[test]
-// fn copy_me_test() {
-//     let str1 = String::from("foo");
-//     assert_eq!(str1, copy_me(/* Change in here only*/ str1));
-// }
+#[test]
+fn copy_me_test() {
+    let str1 = String::from("foo");
+    assert_eq!(str1, copy_me(/* Change in here only*/ &str1));
+}
 
-// #[test]
-// fn copy_me_test2() {
-//     let str1 = String::from("foo");
-//     let str2 = copy_me(str1 /* Change in here only*/);
-//     assert_eq!(str1, str2);
-// }
+#[test]
+fn copy_me_test2() {
+    let str1 = String::from("foo");
+    let str2 = copy_me(&str1 /* Change in here only*/);
+    assert_eq!(str1, str2);
+}
 
 /*
     Problem 4: Lifetime specifiers
@@ -84,18 +101,23 @@ fn duplicate_string(s: &str, times: usize) -> Vec<String> {
     such as "unsafe code" can be used to turn off Rust's safety and lifetime
     checks.)
 */
-// fn new_ref_string() -> &String {
-//     unimplemented!();
+// fn new_ref_string() -> &'static String {
+//     // const STATIC_STR: &'static String = &String::from("Hi");
+//     // STATIC_STR
 // }
 
-// fn new_ref_str() -> &str {
-//     unimplemented!();
-// }
+fn new_ref_str() -> &'static str {
+    "hi"
+}
 
 // The same function from part2
-// fn pick_longest2(s1: &str, s2: &str) -> &str {
-//     unimplemented!()
-// }
+fn pick_longest2<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() >= s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
 
 /*
     Problem 5: Using functions with lifetimes
@@ -108,11 +130,40 @@ fn duplicate_string(s: &str, times: usize) -> Vec<String> {
 */
 
 fn pick_longest_in_v1(v: Vec<String>) -> String {
-    unimplemented!()
+    let mut longest = String::from("");
+    for s in v {
+        let curr_longest = &longest;
+        longest = pick_longest2(curr_longest, &s).to_string();
+    }
+    longest
 }
 
 fn pick_longest_in_v2(v: Vec<&str>) -> &str {
-    unimplemented!()
+    let mut longest : &str = "";
+    for s in v {
+        let curr_longest = longest;
+        longest = pick_longest2(curr_longest, s);
+    }
+    longest
+}
+
+#[test]
+fn test_pick_longest_in_v1() {
+    assert_eq!(
+        pick_longest_in_v1(vec!["cat".to_string(), "dog".to_string(), "cockroach".to_string()]),
+        "cockroach".to_string()
+    );
+}
+
+#[test]
+fn test_pick_longest_in_v2() {
+    assert_eq!(
+        pick_longest_in_v2(vec!["cat", "dog", "cockroach"]),
+        "cockroach"
+    );
+    assert_eq!(
+        pick_longest_in_v2(vec![]), ""
+    );
 }
 
 /*
@@ -127,18 +178,28 @@ fn pick_longest_in_v2(v: Vec<&str>) -> &str {
 */
 
 fn pad_with_zeros_v1(v: Vec<usize>, desired_len: usize) -> Vec<usize> {
-    unimplemented!()
-    // debug_assert_eq!(result.len(), desired_len);
+    assert!(v.len() <= desired_len);
+    let pad_len : usize = desired_len - v.len();
+    let pad = vec![0; pad_len];
+    let result = [v, pad].concat();
+    debug_assert_eq!(result.len(), desired_len);
+    result
 }
 
 fn pad_with_zeros_v2(slice: &[usize], desired_len: usize) -> Vec<usize> {
-    unimplemented!()
-    // debug_assert_eq!(result.len(), desired_len);
+    assert!(slice.len() <= desired_len);
+    let pad_len : usize = desired_len - slice.len();
+    let mut pad = Vec::new();
+    pad.resize(pad_len, 0);
+    let result = [slice, &pad].concat();
+    debug_assert_eq!(result.len(), desired_len);
+    result
 }
 
 fn pad_with_zeros_v3(v: &mut Vec<usize>, desired_len: usize) {
-    unimplemented!()
-    // debug_assert_eq!(result.len(), desired_len);
+    assert!(v.len() <= desired_len);
+    v.resize(desired_len, 0);
+    debug_assert_eq!(v.len(), desired_len);
 }
 
 #[test]
@@ -183,12 +244,17 @@ fn test_pad_twice_v3() {
 */
 
 fn append_row(grid: &mut Vec<Vec<bool>>, row: Vec<bool>) {
-    unimplemented!()
+    grid.push(row)
 }
 
 fn is_first_row(grid: &[Vec<bool>], row: &[bool]) -> bool {
     // Check if row is the first row in grid
     // Remember to handle the case when grid is empty
+    if grid.is_empty() {
+        false
+    } else {
+        grid[0] == row
+    }
 }
 
 /*
@@ -207,18 +273,30 @@ use std::collections::HashMap;
 // https://doc.rust-lang.org/std/collections/struct.HashMap.html
 
 fn vector_to_hashmap(v: &[(i32, String)]) -> HashMap<i32, String> {
-    unimplemented!();
+    let mut res = HashMap::new();
+    for (k, s) in v {
+        res.insert(*k, s.clone());
+    }
+    res
 }
 
 // Now rewrite this function to delete all entries in hashmap where the keys
 // are negative.
 fn delete_negative_keys(h: &mut HashMap<i32, i32>) {
     // This fails, uncomment to see error.
-    // for k in h.keys() {
-    //     if *k < 0 {
-    //         h.remove(k);
-    //     }
-    // }
+    let keys : Vec<i32> = h.keys().copied().filter(|k| k < &0).collect();
+    for k in keys {
+        h.remove(&k);
+    }
+}
+
+#[test]
+fn test_delete_negative_keys() {
+    let mut v = HashMap::new();
+    v.insert(-1, 2);
+    v.insert(2, 2);
+    delete_negative_keys(&mut v);
+    assert_eq!(v.len(), 1);
 }
 
 /*
@@ -244,5 +322,9 @@ fn merge_maps(
     merged: &mut HashMap<String, String>,
     add: HashMap<String,String>
 ) {
-    unimplemented!()
+    for (k, v) in add {
+        merged.entry(k)
+            .and_modify(|e| e.push_str(&v))
+            .or_insert(v);
+    }
 }
